ef274332345472075d488025da358a358316814e
Ryann F <ryannnkl@gmail.com>
2020-07-30 23:11:21 -0300

    updated more functional metod sendMessage

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 import time
 
 
 class Chatbot:
-    def __init__(self, user):
-        self.name = user
+    def __init__(self):
         options = webdriver.ChromeOptions()
         options.add_argument("lang=pt-br")
         self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
@@ -14,43 +17,38 @@ class Chatbot:
     def openBrowser(self):
         print("[] opening web site")
         self.driver.get("https://web.whatsapp.com")
-        time.sleep(30)
-        person = self.driver.find_element_by_xpath(
-            f"//span[@title='{self.name}']")
-        time.sleep(5)
+        self.driver.maximize_window()
+
+    def searchContact(self, name):
+        person = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located(
+                (By.XPATH, f"//span[@title='{name}']")))
         person.click()
         print("[] Usuario encontrado")
-        time.sleep(3)
 
     def SendSimpleMessage(self, message):
-        # <span dir="auto" title="Sininho" class="_3ko75 _5h6Y_ _3Whw5">
-        # <div tabindex="-1" class="_3uMse">
-        # <span data-testid="send" data-icon="send" class="">
-        input_chat = self.driver.find_element_by_class_name('_3uMse')
-        time.sleep(1)
+        input_chat = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, '_3uMse')))
         input_chat.click()
-        print("digitando")
-        input_chat.send_keys(message)
-        send_button = self.driver.find_element_by_xpath(
-            "//span[@data-icon='send']")
-        time.sleep(1)
-        send_button.click()
-        print("menssagem enviada")
-        self.driver.quit()
+        # input_chat = self.driver.find_element_by_class_name('_3uMse')
+        print("[] writing...")
+        input_chat.send_keys(message + Keys.RETURN)
+        time.sleep(2)
+        # send_button = self.driver.find_element_by_xpath("//span[@data-icon='send']")
+        # send_button = WebDriverWait(self.driver, 3).until(
+        #     EC.presence_of_element_located(
+        #         (By.XPATH, f"//span[@data-icon='send']")))
+        # send_button.click()
+        print("[] Sended message!")
 
     def SendLoopMessage(self, message, count):
         for i in range(count):
-            input_chat = self.driver.find_element_by_class_name('_3uMse')
-            print(f"[{i}] writing")
+            input_chat = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, '_3uMse')))
+            input_chat.click()
+            print(f"[{i+1}] writing...")
             time.sleep(0.5)
             input_chat.send_keys(message + Keys.RETURN)
             time.sleep(0.5)
-            send_button = self.driver.find_element_by_xpath(
-                "//span[@data-icon='send']")
-            time.sleep(0.5)
-            send_button.click()
-            time.sleep(0.3)
-
             print("[] sending message")
         print("[] end of messages")
-        self.driver.quit()
--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from bot import Chatbot
 
+
+def initial(bot):
+
+    name = input("Digite o nome do usuario para procurar\n:")
+    bot.searchContact(name)
+
+    message = input("Digite a menssagem para ser exibida\n:")
+
+    metod = int(
+        input(
+            "Digite uma opção:\nMenssagem simples [1]\nmultiplos envios [2]\n: "
+        ))
+
+    if metod == 1:
+        bot.SendSimpleMessage(message)
+    if metod == 2:
+        count = int(input("digite a quantidade de vezes"))
+        bot.SendLoopMessage(message, count)
+    else:
+        print("Opção invalida")
+
+
 print("[] initialized bot")
 
-name = input("Digite o nome do usuario para procurar\n:")
-
-message = input("Digite a menssagem para ser exibida\n:")
-
-bot = Chatbot(name)
+bot = Chatbot()
 bot.openBrowser()
-bot.SendLoopMessage(message, 100)
+
+while True:
+    initial(bot)
+    if (input("Continuar? S/N: ").upper() == "S"):
+        continue
+    else:
+        bot.driver.quit()
+        break
 
 print("[] finalized bot")
